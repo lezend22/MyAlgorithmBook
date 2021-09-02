f4276ca303f23a513245951f87f0651a28996c3e
lezend22 <lezend22@gmail.com>
2021-09-02 19:40:06 +0900

    add 떡볶이떡만들기

---
+++ 이진탐색/떡볶이떡만들기.py
@@ -0,0 +1,45 @@
+import sys
+
+
+n, m = map(int, sys.stdin.readline().split())
+arr = list(map(int, sys.stdin.readline().split()))
+
+### 반복문
+# start = 0
+# end = max(arr)
+# result = 0
+
+# while(start <= end):
+#     total = 0
+#     mid = (start + end) // 2
+#
+#     for i in arr:
+#         if i > mid:
+#             total += (i-mid)
+#
+#     if total < m:
+#         end = mid - 1
+#     else:
+#         result = mid
+#         start = mid + 1
+#
+# print(result)
+
+###재귀
+def binarySearch(arr, target, start, end):
+    if start > end:
+        return None
+    total = 0
+    mid = (start + end) // 2
+    for i in arr:
+        if i > mid:
+            total += (i - mid)
+    if total < target:
+        binarySearch(arr, target, start, mid-1)
+    else:
+        global result
+        result = mid
+        binarySearch(arr, target, mid+1, end)
+
+binarySearch(arr, m, 0, max(arr))
+print(result)
