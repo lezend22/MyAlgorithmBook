70d48268351a6fbcbd44f5233bab6986de597d38
lezend22 <lezend22@gmail.com>
2021-09-03 21:45:58 +0900

    add 바닥공사

---
+++ 다이나믹프로그래밍/효율적인화폐구성.py
@@ -0,0 +1,44 @@
+import sys
+
+arr = list()
+n, m = map(int, sys.stdin.readline().split())
+for i in range(n):
+    arr.append(int(sys.stdin.readline()))
+
+d = [0] * 10001
+for i in range(0, m+1):
+    if i in arr:
+        d[i] = 1
+
+    p = list()
+    for x in arr:
+        sign = i - x
+        if sign < 0:
+            sign = 0
+        p.append(d[sign])
+    d[i] = min(p) + 1
+
+print(d[m])
+
+
+
+
+### 잘못풀었어
+# for i in range(0, m+1):
+#     check = 0
+#     if i in arr:
+#         d[i] = 1
+#
+#     for x in arr:
+#         sign = (i - x)
+#         if sign < 0:
+#             sign = 0
+#         check += sign
+#         d[i] += d[sign]
+#
+#     if check == i:
+#         d[i] = d[i] // 2
+#     print("i =", i, "d[i] = ", d[i])
+#
+# print(d[m])
+
